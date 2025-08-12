f36ec3e9a5f7763f2026f5b9829e5a2697d42be6
Wouter.Saelens <wouter.saelens@gmail.com>
2025-08-12 11:42:03 +0200

    cleanup

--- code/commands_wouter.sh
+++ code/commands_wouter.sh
@@ -4,3 +4,5 @@ python code/convert_notebook.py code/ZZ_Endogenous/2-create_figures.py code/ZZ_E
 python code/convert_notebook.py code/ZZ_Dose_comparison/dose_comparison.py code/ZZ_Dose_comparison/README.md
 
 jupyter nbconvert code/ZZ_Power/power.ipynb   --to markdown   --output README   --output-dir code/ZZ_Power   --ExtractOutputPreprocessor.dir=".figures"
+
+jupyter nbconvert code/ZZ_Scaling/scaling.ipynb   --to markdown   --output README   --output-dir code/ZZ_Scaling   --ExtractOutputPreprocessor.dir=".figures"
